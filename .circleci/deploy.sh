3ff92cd832efccdb1ca4d012f96babc5d946371c
Alex Maingot <alex.maingot@gmail.com>
2020-07-28 16:28:52 -0500

    Fix the curl

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-curl --X POST \
-  --url https://spinnaker-api.kazoohr.io/webhooks/webhook/hackathon-2020 \
-  --H 'content-type: application/json'
-  --d '{
+curl -X POST \
+  https://spinnaker-api.kazoohr.io/webhooks/webhook/hackathon-2020 \
+  -H 'content-type: application/json'
+  -d '{
       "SECRET": "'$SPINNAKER_SECRET'",
       "parameters": {
         "git_ref": "'$CIRCLE_SHA1'"
