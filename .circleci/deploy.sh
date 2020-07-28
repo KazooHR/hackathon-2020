f730429371dac99228d3d1a291ff9f7f1a44b1c7
Alex Maingot <alex.maingot@gmail.com>
2020-07-28 16:37:01 -0500

    More crap

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+$PAYLOAD=`echo '{
+  "SECRET": "'$SPINNAKER_SECRET'",
+  "parameters": {
+    "git_ref": "'$CIRCLE_SHA1'"
+  }
+}'`
+
 curl -X POST \
   https://spinnaker-api.kazoohr.io/webhooks/webhook/hackathon-2020 \
   -H 'content-type: application/json'
-  -d '{
-      "SECRET": "'$SPINNAKER_SECRET'",
-      "parameters": {
-        "git_ref": "'$CIRCLE_SHA1'"
-      }
-    }'
+  -d "$PAYLOAD"
