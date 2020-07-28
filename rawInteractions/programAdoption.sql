cc87e0c1cdc96f48773d90275af6ca31029811f2
Wendy Kuhn <wendy.kuhn@youearnedit.com>
2020-07-28 16:08:05 -0500

    only YEI active users

--- rawInteractions/programAdoption.sql
+++ rawInteractions/programAdoption.sql
@@ -8,13 +8,12 @@ WHERE A.action='user_sign_in' AND A.user_id=$1;
 SELECT _id, last_sign_in_at FROM cdc_prod.RR_users
 WHERE _id=$1;
 
-# average number of sign in activities for active users in active companies
+# average number of sign in activities for active users in YEI
 SELECT avg(number_sign_ins), max(number_sign_ins), min(number_sign_ins) FROM 
 (
 SELECT count(A._id) AS number_sign_ins FROM cdc_prod.RR_activities AS A
 JOIN cdc_prod.RR_users AS USER on A.user_id=USER._id
-JOIN cdc_prod.RR_companies AS COMPANY on USER.company_id=COMPANY._id
-WHERE COMPANY.status='active'
+WHERE A.company_id='52e9e22f549d52e7a1000001'
 AND USER.active=true
 AND A.action='user_sign_in'
 GROUP BY USER._id
