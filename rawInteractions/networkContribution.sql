899f2bbeccc8becc5fd19d6cb0e7524ce331ea0b
Madeline Ryder <mryder@highground.com>
2020-07-29 14:56:49 -0500

    team player

--- rawInteractions/networkContribution.sql
+++ rawInteractions/networkContribution.sql
@@ -4,6 +4,6 @@ SELECT avg(count) AS Avg, MIN(count) AS Min, MAX(count) AS Max
     (
     SELECT COUNT (*) AS Count
       FROM `kazoo-production.cdc_prod.PM_Recognition`
-     WHERE IssuerGroupId = "1d277e60-9cd9-11e2-ba5b-8bde9b7bbcde"
-    GROUP BY IssuerMemberId
+     WHERE "CreatorMember.GroupId" = "1d277e60-9cd9-11e2-ba5b-8bde9b7bbcde"
+    GROUP BY "CreatorMember.hgId"
     ) as counts
