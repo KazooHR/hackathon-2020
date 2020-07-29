a48ae354a83c847db28769bcaea7fe12b8ce77c7
Alex Maingot <alex.maingot@gmail.com>
2020-07-29 09:21:44 -0500

    Correct redirectUri for prod/local

--- web/src/utils/auth.ts
+++ web/src/utils/auth.ts
@@ -1,9 +1,14 @@
 import { AuthService } from "@okta/okta-react";
+import config from "./config";
+
+const redirectUri = config.IS_PROD
+  ? `https://${window.location.host}/authorization-code/callback`
+  : "http://localhost:3000/authorization-code/callback";
 
 const auth = new AuthService({
   clientId: "0oa4wqluetAkekgqg357",
   issuer: "https://kazoohr.okta.com",
-  redirectUri: "http://localhost:3000/authorization-code/callback",
+  redirectUri,
   scopes: ["openid", "profile", "email"],
   pkce: true,
 });
