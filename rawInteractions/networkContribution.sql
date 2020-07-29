bd8c05320e03640b0679dffbf1a4478f8cbaa02f
Madeline Ryder <mryder@highground.com>
2020-07-29 14:49:30 -0500

    social amp

--- rawInteractions/networkContribution.sql
+++ rawInteractions/networkContribution.sql
@@ -3,7 +3,7 @@ SELECT avg(count) AS Avg, MIN(count) AS Min, MAX(count) AS Max
   FROM
     (
     SELECT COUNT (*) AS Count
-      FROM `kazoo-production.cdc_prod.PM_RecognitionRequest`
+      FROM `kazoo-production.cdc_prod.PM_Recognition`
      WHERE IssuerGroupId = "1d277e60-9cd9-11e2-ba5b-8bde9b7bbcde"
     GROUP BY IssuerMemberId
     ) as counts
