9522c6b89581c165254c2801ce690aca4629ab8b
Alex Maingot <alex.maingot@gmail.com>
2020-07-29 15:23:46 -0500

    Team player

--- rawInteractions/teamPlayer.sql
+++ rawInteractions/teamPlayer.sql
@@ -1,11 +1,11 @@
 --# of Recognition Received/Avg. of Recognitions Received in Company
-
 -- PM
-SELECT avg(count) AS Avg, MIN(count) AS Min, MAX(count) AS Max
-  FROM
-    (
+SELECT avg(count) AS Avg,
+  MIN(count) AS Min,
+  MAX(count) AS Max
+FROM (
     SELECT COUNT (*) AS Count
-      FROM `kazoo-production.cdc_prod.PM_Recognition`
-     WHERE "RecipientMember.GroupId" = "1d277e60-9cd9-11e2-ba5b-8bde9b7bbcde"
+    FROM `kazoo-production.cdc_prod.PM_Recognition`
+    WHERE "RecipientMember.GroupId" = "1d277e60-9cd9-11e2-ba5b-8bde9b7bbcde"
     GROUP BY "RecipientMember.hgId"
-    ) as counts
+  ) as counts
