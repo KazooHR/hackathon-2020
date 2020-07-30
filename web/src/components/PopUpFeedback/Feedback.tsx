2fac503f190310bbd111b211c91424c262ac5371
Madeline Ryder <mryder@highground.com>
2020-07-30 17:19:56 -0500

    comment

--- web/src/components/PopUpFeedback/Feedback.tsx
+++ web/src/components/PopUpFeedback/Feedback.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Card, Button, Header, Text, Flex, Spacer, TextArea } from "@kazoohr/confetti";
+import { Card, Button, Header, Text, Flex, Spacer, TextArea, Grid } from "@kazoohr/confetti";
 import { Rating } from "@material-ui/lab";
 
 export interface PopUpFeedbackProps {
@@ -18,45 +18,41 @@ export function PopUpFeedback({feedback}: {feedback: PopUpFeedbackProps}) {
     const [rating, setRating] = useState<number>(2.5);
 
     return (
-        <Card
-            barBrandingClass=""
-            barColor=""
-            barLocation="top"
-            className=""
-            withBorder
-        >
-            <Header level="3" size="small">{feedback.action}</Header>
-            <Header level="1" size="medium" thick>{feedback.subject}</Header>
-            <Text>{feedback.question}{" "}<b>{feedback.value}</b>{"?"}</Text>
-            <Flex>
-                <Rating style={{fontSize: "64px", color: "#2A5CDB"}} name="half-rating" defaultValue={rating} precision={0.5} size="large"
-                        onChangeActive={(event: object, value: number) => setRating(value)} />
-            </Flex>
-            {!commentOpen &&
-            <Button onClick={() => setCommentOpen(true)}>
-                Add a comment (optional)
-            </Button>}
-            {commentOpen && <TextArea
-                error=""
-                maxLength={-1}
-                minRows={-1}
-                onMention={null}
-                optionalLabelText="(optional)"
-                placeholder={`let ${feedback.subject} know why you chose this rating`}
-                speechBubble={false}
-                value={comment}
-                onChange={(comment: string) => setComment(comment)}
-            />}
-            <Spacer size="medium" orientation="horizontal" />
-            <Text>{"Your feedback is shared with Neha anonymously."}</Text>
-            <Spacer size="medium" orientation="horizontal" />
+        <Card>
+            <Grid>
+                <Header level="3" size="small">{feedback.action}</Header>
+                <Header level="1" size="medium" thick>{feedback.subject}</Header>
+                <Text>{feedback.question}{" "}<b>{feedback.value}</b>{"?"}</Text>
+                <Flex>
+                    <Rating style={{fontSize: "64px", color: "#2A5CDB"}} name="half-rating" defaultValue={rating} precision={0.5} size="large"
+                            onChangeActive={(event: object, value: number) => setRating(value)} />
+                </Flex>
+                {!commentOpen &&
+                <Button onClick={() => setCommentOpen(true)}>
+                    Add a comment (optional)
+                </Button>}
+                {commentOpen && <TextArea
+                    error=""
+                    maxLength={-1}
+                    minRows={-1}
+                    onMention={null}
+                    optionalLabelText="(optional)"
+                    placeholder={`let ${feedback.subject} know why you chose this rating`}
+                    speechBubble={false}
+                    value={comment}
+                    onChange={(comment: string) => setComment(comment)}
+                />}
+                <Spacer size="medium" orientation="horizontal" />
+                <Text>{"Your feedback is shared with Neha anonymously."}</Text>
+                <Spacer size="medium" orientation="horizontal" />
 
-            <Button onClick={null}>
-                Submit Feedback
-            </Button>
-            <Button onClick={null}>
-                Remind me later
-            </Button>
+                <Button onClick={null}>
+                    Submit Feedback
+                </Button>
+                <Button onClick={null}>
+                    Remind me later
+                </Button>
+                </Grid>
         </Card>
             )
 }
