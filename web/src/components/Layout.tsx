02a899c1a84c58deff1586fced834f6b7c82eafa
Wendy Kuhn <wendy.kuhn@youearnedit.com>
2020-07-30 09:24:41 -0500

    mui theme

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -6,22 +6,26 @@ import {
   Toolbar,
   Typography,
   CssBaseline,
+  ThemeProvider
 } from "@material-ui/core";
+import theme from './muiTheme';
 
 const Layout: React.FC = (props) => {
   const { children } = props;
 
   return (
     <>
-      <CssBaseline />
-      <AppBar color="primary" position="static" style={{ height: 64 }}>
-        <Toolbar style={{ height: 64 }}>
-          <Typography variant="h5" color="inherit">
-            Talent IQ
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Container maxWidth="sm">{children as any}</Container>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar color="primary" position="static" style={{ height: 64 }}>
+          <Toolbar style={{ height: 64 }}>
+            <Typography variant="h5" color="inherit">
+              Talent IQ
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth="sm">{children as any}</Container>
+        </ThemeProvider>
     </>
   );
 };
---
+++ web/src/components/muiTheme.ts
@@ -0,0 +1,16 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#227c31',
+      contrastText: '#FFF',
+    },
+    secondary: {
+      main: '#fd5d4b',
+      contrastText: '#333',
+    }
+  },
+});
+
+export default theme;
