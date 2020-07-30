f8fe627c4bae0104643fcab6094092783e6e946c
Wendy Kuhn <wendy.kuhn@youearnedit.com>
2020-07-30 18:37:05 -0500

    chart card

---
+++ web/src/components/AdminDashboardChart.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Card } from '@kazoohr/confetti';
+
+const AdminDashboardChart: React.FC = () => {
+
+  return (
+    <Card
+      className=""
+      withBorder
+      noShadow
+    >
+      <p>Cartesian Chart</p>
+    </Card>
+  );
+};
+
+export default AdminDashboardChart;
