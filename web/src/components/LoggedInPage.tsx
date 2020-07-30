d1ba7dbcff690735bdc086df75f4aace3c3b1266
Wendy Kuhn <wendy.kuhn@youearnedit.com>
2020-07-30 12:17:14 -0500

    web logo

--- web/src/components/LoggedInPage.tsx
+++ web/src/components/LoggedInPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Paper, Typography } from "@material-ui/core";
+import { CardMedia, Paper, Typography } from "@material-ui/core";
 
 import { useHelloWorldQuery } from "../graphql/hooks";
 
@@ -8,7 +8,12 @@ const LoggedInPage: React.FC = () => {
 
   return (
     <Paper style={{ margin: 16, padding: 16 }}>
-      <Typography align="center" variant="h1">
+      <Typography align="center" variant="h1" color="secondary">
+        <CardMedia 
+          style={{ height: 50, width: 'auto', margin: '0 auto' }} 
+          component={'img'} 
+          image={require('web/public/web_logo.png')}
+        />
         Welcome {data?.hello || "Friend"}!
       </Typography>
     </Paper>
