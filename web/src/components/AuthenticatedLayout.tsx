b8133d2d983cd92c760e19eaf4dc29889417770b
Kelley Shanahan <shanahangirl@gmail.com>
2020-07-31 17:10:51 -0500

    Remove img

--- web/src/components/AuthenticatedLayout.tsx
+++ web/src/components/AuthenticatedLayout.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
 
+import { Text, Button, IconButton } from "@kazoohr/confetti";
+
 import {
+  AppBar,
   Container,
+  Toolbar,
+  Typography,
   CssBaseline,
-  ThemeProvider,
+  ThemeProvider
 } from "@material-ui/core";
 import theme from "./muiTheme";
 
@@ -12,11 +17,32 @@ const AuthenticatedLayout: React.FC = (props) => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <img 
-        src="./1440.png" 
-        style={{ position: 'absolute', width: '1440px', height: 'auto'}}
-        alt=""
-      />
+      <AppBar color="primary" position="static" style={{ height: 64 }}>
+        <Toolbar style={{ height: 64 }}>
+            <IconButton
+              icon="hamburger"
+              iconSize={24}
+              className={"menuButton"}
+            />
+            <Typography variant="h5" color="inherit">
+              Talent IQ Demo
+            </Typography>
+            <Button
+              variant={"feature_inverted"}
+              href={"/admin"}
+              className={"adminButton"}
+              size="large"
+            >
+              <Text
+                className={""}
+                size={"small"}
+                data-testid="admin-link-text"
+              >
+                {"admin"}
+              </Text>
+            </Button>
+          </Toolbar>
+      </AppBar>
       <Container maxWidth="lg">{children as any}</Container>
     </ThemeProvider>
   );
