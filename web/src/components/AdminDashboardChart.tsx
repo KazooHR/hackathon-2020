da26c435adff42f589d742c25825abdcecc22f41
Wendy Kuhn <wendy.kuhn@youearnedit.com>
2020-07-31 10:48:23 -0500

    skills driver img

--- web/src/components/AdminDashboardChart.tsx
+++ web/src/components/AdminDashboardChart.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { CardMedia } from "@material-ui/core";
 import { Card } from "@kazoohr/confetti";
 
 const AdminDashboardChart: React.FC = () => {
   return (
-    <Card className="" withBorder noShadow>
-      <CardMedia
-        style={{ width: 700, height: 500 }}
-        image={require("web/public/skills-driver.png")}
-      />
+    <Card 
+      className="" 
+      withBorder 
+      noShadow
+      style={{ margin: 0, padding: 0 }}
+    >
+      <img src="./skills-driver.png" style={{ width: '900px', height: 'auto'}}/>
     </Card>
   );
 };
